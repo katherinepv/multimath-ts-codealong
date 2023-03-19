3fe18f7fe85e5a211e8695096a026c48410aaee2
katherine <pv.katherine7@gmail.com>
2023-03-19 20:48:58 +0000

    implemented use of function types

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,56 @@
-const startGame = () => {
-    const messagesElement = document.getElementById('messages');
-    messagesElement!.innerText = 'Welcome to MultiMath! Starting a new game...';
-}
+const logPlayer = (name: string = "MultiMath Player"): void => {
+  console.log(`New game starting for player: ${name}`);
+};
 
-document.getElementById('startGame')!.addEventListener('click', startGame);
+const logMessage = (message: string) => console.log(message);
+const logError = (err: string): void => console.error(err);
+
+const getInputValue = (elementID: string): string | undefined => {
+  // <HTMLInputElement> is a type assertion that will treat the element it finds as
+  // the more specific HTMLInputElement type.
+  const inputElement: HTMLInputElement = <HTMLInputElement>(
+    document.getElementById(elementID)
+  );
+
+  if (inputElement.value === "") {
+    return undefined;
+  } else {
+    return inputElement.value;
+  }
+};
+
+const postScore = (
+  score: number,
+  playerName: string = "MultiMath Player"
+): void => {
+  // TS functions have a type of their own
+  // using function types as type annotations
+
+  // to gove this variable a function type, just add a colon after the variable name as usual
+  //and then put the expected function parameters and their types in parentheses.
+  // followed by an arrow and the expected function return type.
+  // this logger variable may now be assigned any function that takes a single string parameter and returns void. (just like logMessage and logError functions).
+  let logger: (value: string) => void;
+
+  if (score < 0) {
+    logger = logError;
+  } else {
+    logger = logMessage;
+  }
+
+  const scoreElement: HTMLElement | null =
+    document.getElementById("postedScores");
+  scoreElement!.innerText = `${score} - ${playerName}`;
+
+  logger(`Score: ${score}`);
+};
+
+const startGame = () => {
+  let playerName: string | undefined = getInputValue("playername");
+  logPlayer(playerName);
+
+  postScore(80, playerName);
+  postScore(-3, playerName);
+};
+
+document.getElementById("startGame")!.addEventListener("click", startGame);
--- js/app.js
+++ js/app.js
@@ -1,7 +1,36 @@
-"use strict";
-var startGame = function () {
-    var messagesElement = document.getElementById('messages');
-    messagesElement.innerText = 'Welcome to MultiMath! Starting a new game...';
+var logPlayer = function (name) {
+    if (name === void 0) { name = "MultiMath Player"; }
+    console.log("New game starting for player: ".concat(name));
 };
-document.getElementById('startGame').addEventListener('click', startGame);
+var logMessage = function (message) { return console.log(message); };
+var logError = function (err) { return console.error(err); };
+var getInputValue = function (elementID) {
+    var inputElement = (document.getElementById(elementID));
+    if (inputElement.value === "") {
+        return undefined;
+    }
+    else {
+        return inputElement.value;
+    }
+};
+var postScore = function (score, playerName) {
+    if (playerName === void 0) { playerName = "MultiMath Player"; }
+    var logger;
+    if (score < 0) {
+        logger = logError;
+    }
+    else {
+        logger = logMessage;
+    }
+    var scoreElement = document.getElementById("postedScores");
+    scoreElement.innerText = "".concat(score, " - ").concat(playerName);
+    logger("Score: ".concat(score));
+};
+var startGame = function () {
+    var playerName = getInputValue("playername");
+    logPlayer(playerName);
+    postScore(80, playerName);
+    postScore(-3, playerName);
+};
+document.getElementById("startGame").addEventListener("click", startGame);
 //# sourceMappingURL=app.js.map
